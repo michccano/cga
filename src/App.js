3b7a831ec146a02f5b401cf8ab6615f8f1576f9c
michccano <68267949+michccano@users.noreply.github.com>
2021-05-22 10:03:13 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "../node_modules/jquery/dist/jquery.min.js";
 
 import "./css/Landingpage.css";
 import Landingpage from "./Landingpage";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, HashRouter as Router } from "react-router-dom";
 import Userproflie from "./Userproflie";
 import Singleart from "./Singleart";
 import "./css/responsive.css";
@@ -14,10 +14,10 @@ import GalleryPage from "./GalleryPage";
 function App() {
     return (
         <>
-            <Route exact path="/" component={Landingpage} />
-            <Route exact path="/Userproflie" component={Userproflie} />
-            <Route exact path="/Singleart" component={Singleart} />
-            <Route exact path="/Gallery" component={GalleryPage}/>
+            <Router exact path="/" component={Landingpage} />
+            <Router exact path="/Userproflie" component={Userproflie} />
+            <Router exact path="/Singleart" component={Singleart} />
+            <Router exact path="/Gallery" component={GalleryPage}/>
         </>
     );
 }
